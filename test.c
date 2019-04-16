2cad3ef7f9f5c115f45890f1daa6b0de3e4f55cc
dkl416 <dkl416@naver.com>
2019-04-17 00:11:09 +0900

    pthread test

--- test.c
+++ test.c
@@ -10,9 +10,34 @@
 #include <pthread.h>
 #include <netdb.h>
 
+int srv(){
+	printf("recieve\n");
+}
+
+int cli(){
+	while(1){
+		printf("send\n");
+		sleep(1);
+	}
+}
+
 int main(int argc, char* argv[]){
 
+  pthread_t sender;
+  int threadId;
+  int status;
 
+  threadId = pthread_create(&sender, NULL, cli, NULL);
 
-   return 0;
+  if(threadId < 0){
+	perror("thread create error : ");
+	exit(0);
+  }
+
+  while(1){
+	srv();
+	sleep(1);
+  }
+  
+  return 0;
 }
