079e2da2b81534470221f18974bb689d2e4e02e5
INBAC <heawtrody@gmail.com>
2019-04-16 22:40:10 +0900

    Edit progress.c

--- progress.c
+++ progress.c
@@ -1,1 +1,27 @@
+/*
+	Written By: Jongwon Lee
+	Purpose: To simulate routing table protocol
+*/
 #include<stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <netdb.h>
+
+#define NODE_NUMBER 5
+
+typedef struct ROUTING_TABLE_ENTRY
+{
+	char* dstip;
+	int dstport;
+	char* nextip;
+	int nextport;
+	int metric;
+};
+
