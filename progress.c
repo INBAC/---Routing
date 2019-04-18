1ec9f37b7b46984085cd6c6cc18d9d0b7b0cfeb4
INBAC <heawtrody@gmail.com>
2019-04-18 19:21:59 +0900

    Implemented socket programming so that it is able to send and receive string

--- progress.c
+++ progress.c
@@ -1,7 +1,7 @@
 /*
 	Written By: Jongwon Lee
 	Purpose: To simulate routing table protocol
-	useful functions (socket, pthread, sigaction, timer, open, close, send, recv, bind, accept, listen, connect, memset, htons/l, ntohs/l, memset, malloc/free)
+	useful functions (socket, pthread, sigaction, timer, open, close, client, recv, bind, accept, listen, connect, memset, htons/l, ntohs/l, memset, malloc/free)
 */
 #include<stdio.h>
 #include <stdlib.h>
@@ -16,8 +16,8 @@
 #include <netdb.h>
 
 #define NODE_NUMBER 5
-#define BUFFER_SIZE 8
-#define PORT_NUM 3605
+#define BUFFER_SIZE 1024
+#define PORT_NUM 9898
 
 typedef struct
 {
@@ -28,29 +28,84 @@ typedef struct
 	int metric;
 }ROUTING_TABLE_ENTRY;
 
-void *sendThreadFunction(void *args)
+void *clientThreadFunction(void *arg)
 {
+	char* ipAddress = (char *)arg;
+	char* sendBuffer;
+	int sendBufferLength;
+	struct sockaddr_in clientSocketAddress;
+	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if(clientSocket == -1)
+	{
+		perror("Client Socket Creation Failure");
+		pthread_exit(NULL);
+	}
+	memset(&clientSocketAddress, 0, sizeof(clientSocketAddress));
+	clientSocketAddress.sin_family = AF_INET;
+	clientSocketAddress.sin_port = htons(PORT_NUM);
+	inet_pton(AF_INET, ipAddress, &clientSocketAddress.sin_addr);
+	printf("Client Waiting For Connection...\n");
+	while(connect(clientSocket, (struct sockaddr *)&clientSocketAddress, sizeof(clientSocketAddress)) == -1);
+//sending
+	sendBuffer = "Im from Test2";
+	sendBufferLength = strlen(sendBuffer);
+	send(clientSocket, sendBuffer, BUFFER_SIZE, 0);
+	printf("Client Sent\n");
+//sending
+	close(clientSocket);
 	pthread_exit(NULL);
 }
 
-void *receiveThreadFunction(void *args)
+void *serverThreadFunction(void *arg)
 {
+	char receiveBuffer[BUFFER_SIZE];
+	int receiveBufferLength;
+	struct sockaddr_in serverSocketAddress;
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	int clientSocket;
+	if(serverSocket == -1)
+	{
+		perror("Server Socket Creation Failure");
+		pthread_exit(NULL);
+	}
+	memset(&serverSocketAddress, 0, sizeof(serverSocketAddress));
+	serverSocketAddress.sin_family = AF_INET;
+	serverSocketAddress.sin_addr.s_addr = htons(INADDR_ANY);
+	serverSocketAddress.sin_port = htons(PORT_NUM);
+	if(bind(serverSocket, (struct sockaddr *)&serverSocketAddress, sizeof(serverSocketAddress)) == -1)
+	{
+		perror("Server Bind Failure");
+		close(serverSocket);
+		pthread_exit(NULL);
+	}
+	if(listen(serverSocket, 0) == -1)
+	{
+		perror("Server Listen Standby Mode Failure");
+		close(serverSocket);
+		pthread_exit(NULL);
+	}
+	clientSocket = accept(serverSocket, (struct sockaddr *)NULL, NULL);
+	if(clientSocket == -1)
+	{
+		perror("Server Connect Accept Failure");
+		close(serverSocket);
+		pthread_exit(NULL);
+	}
+//receive
+	recv(clientSocket, receiveBuffer, BUFFER_SIZE, 0);
+	printf("%s\n", receiveBuffer);
+//receive
+	close(serverSocket);
 	pthread_exit(NULL);
 }
 
 void main(int argc, char* args[])
 {
-	if(argc < 2)
-	{
-		printf("Please enter the IP address");
-		exit(0);
-	}
-	pthread_t sendThread;
-	pthread_t receiveThread;
-	pthread_create(&sendThread, NULL, sendThreadFunction, NULL);
-	pthread_create(&receiveThread, NULL, receiveThreadFunction, NULL);
-
-	pthread_join(sendThread, NULL);
-	pthread_join(receiveThread, NULL);
+	pthread_t clientThread;
+	pthread_t serverThread;
+	pthread_create(&clientThread, NULL, clientThreadFunction, args[1]);
+	pthread_create(&serverThread, NULL, serverThreadFunction, NULL);
+	pthread_join(clientThread, NULL);
+	pthread_join(serverThread, NULL);
 	return;
 }
