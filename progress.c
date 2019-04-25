581cc3e906ccc7732570a6c64b80e9bea1b2dc81
INBAC <heawtrody@gmail.com>
2019-04-25 19:44:55 +0900

    Modified progress.c

--- progress.c
+++ progress.c
@@ -1,9 +1,4 @@
-/*
-	Written By: Jongwon Lee
-	Purpose: To simulate routing table protocol
-	useful functions (socket, pthread, sigaction, timer, open, close, client, recv, bind, accept, listen, connect, memset, htons/l, ntohs/l, memset, malloc/free)
-*/
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
@@ -33,6 +28,7 @@ typedef struct
 typedef struct
 {
 	char destinationIp[IP_LENGTH];
+	char sourceIp[IP_LENGTH];
 	char message[BUFFER_SIZE];
 }PACKET;
 
@@ -56,7 +52,8 @@ char* getIpAddress()
 
 void *clientThreadFunction(void *arg)
 {
-	char* ipAddress = (char *)arg;
+	char** neighborIpAddress = (char **)arg;
+	char* myIpAddress = getIpAddress();
 	char* sendBuffer;
 	PACKET sendPacket;
 	size_t getlineLength;
@@ -70,7 +67,7 @@ void *clientThreadFunction(void *arg)
 	memset(&clientSocketAddress, 0, sizeof(clientSocketAddress));
 	clientSocketAddress.sin_family = AF_INET;
 	clientSocketAddress.sin_port = htons(PORT_NUMBER);
-	inet_pton(AF_INET, ipAddress, &clientSocketAddress.sin_addr);
+	inet_pton(AF_INET, neighborIpAddress, &clientSocketAddress.sin_addr);
 	while(connect(clientSocket, (struct sockaddr *)&clientSocketAddress, sizeof(clientSocketAddress)) == -1);
 //sending
 	while(1)
@@ -79,7 +76,8 @@ void *clientThreadFunction(void *arg)
 		sendBuffer = NULL;
 		getline(&sendBuffer, &getlineLength, stdin);
 		strcpy(sendPacket.message, sendBuffer);
-		strcpy(sendPacket.destinationIp, ipAddress);
+		strcpy(sendPacket.sourceIp, myIpAddress);
+		strcpy(sendPacket.destinationIp, neighborIpAddress);
 		send(clientSocket, &sendPacket, sizeof(PACKET), 0);
 		if(strcmp(sendBuffer, "exit\n") == 0)
 			break;
@@ -91,8 +89,24 @@ void *clientThreadFunction(void *arg)
 
 void *serverThreadFunction(void *arg)
 {
+	int clientSocket = *(int *)arg;
 	char* receiveBuffer = (char *)malloc(sizeof(PACKET));
 	PACKET *receivePacket;
+	while(1)
+	{
+		memset(receiveBuffer, 0, sizeof(PACKET));
+		recv(clientSocket, receiveBuffer, sizeof(PACKET), 0);
+		receivePacket = (PACKET*)receiveBuffer;
+		printf("%s: %s",receivePacket->sourceIp, receivePacket->message);
+	}
+}
+
+void main(int argc, char* args[])
+{
+	int threadNumber = 1;
+	pthread_t clientThread;
+	pthread_t serverThread[NODE_NUMBER];
+	pthread_create(&clientThread, NULL, clientThreadFunction, args + 1);
 	struct sockaddr_in serverSocketAddress;
 	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 	int clientSocket;
@@ -111,41 +125,21 @@ void *serverThreadFunction(void *arg)
 		close(serverSocket);
 		pthread_exit(NULL);
 	}
-	if(listen(serverSocket, 0) == -1)
-	{
-		perror("Server Listen Standby Mode Failure");
-		close(serverSocket);
-		pthread_exit(NULL);
-	}
-	clientSocket = accept(serverSocket, (struct sockaddr *)NULL, NULL);
-	if(clientSocket == -1)
-	{
-		perror("Server Connect Accept Failure");
-		close(serverSocket);
-		pthread_exit(NULL);
-	}
-//receive
 	while(1)
 	{
-		memset(receiveBuffer, 0, sizeof(PACKET));
-		recv(clientSocket, receiveBuffer, sizeof(PACKET), 0);
-		receivePacket = (PACKET*)receiveBuffer;
-		printf("%s", receivePacket->message);
-		if(strcmp(receivePacket->message, "exit\n") == 0)
-			break;
+		if(listen(serverSocket, 0) == -1)
+		{
+			perror("Server Listen Standby Mode Failure");
+			close(serverSocket);
+			exit(0);
+		}
+		clientSocket = accept(serverSocket, (struct sockaddr *)NULL, NULL);
+		if(clientSocket == -1)
+		{
+			perror("Server Connect Accept Failure");
+			close(serverSocket);
+			exit(0);
+		}
+		pthread_create(&serverThread[threadNumber++], NULL, serverThreadFunction, &clientSocket);
 	}
-//receive
-	close(serverSocket);
-	pthread_exit(NULL);
-}
-
-void main(int argc, char* args[])
-{
-	pthread_t clientThread;
-	pthread_t serverThread;
-	pthread_create(&clientThread, NULL, clientThreadFunction, args[1]);
-	pthread_create(&serverThread, NULL, serverThreadFunction, NULL);
-	pthread_join(clientThread, NULL);
-	pthread_join(serverThread, NULL);
-	return;
 }
