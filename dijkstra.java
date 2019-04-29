e72cd670fd75bdd993dbf61f5075a07fc60a7968
INBAC <heawtrody@gmail.com>
2019-04-29 16:35:39 +0900

    Final Submission

--- dijkstra.java
+++ dijkstra.java
@@ -57,55 +57,21 @@ public class dijkstra
 		}
 	}
 	
-    // Function that implements Dijkstra's 
-    // single source shortest path 
-    // algorithm for a graph represented  
-    // using adjacency matrix 
-    // representation 
     private static void dijkstra(int[][] adjacencyMatrix, int startVertex) 
     { 
         int nVertices = adjacencyMatrix[0].length; 
-  
-        // shortestDistances[i] will hold the 
-        // shortest distance from src to i 
         int[] shortestDistances = new int[nVertices]; 
-  
-        // added[i] will true if vertex i is 
-        // included / in shortest path tree 
-        // or shortest distance from src to  
-        // i is finalized 
         boolean[] added = new boolean[nVertices]; 
-  
-        // Initialize all distances as  
-        // INFINITE and added[] as false 
         for (int vertexIndex = 0; vertexIndex < nVertices; vertexIndex++) 
         { 
             shortestDistances[vertexIndex] = Integer.MAX_VALUE; 
             added[vertexIndex] = false; 
         } 
-          
-        // Distance of source vertex from 
-        // itself is always 0 
         shortestDistances[startVertex] = 0; 
-  
-        // Parent array to store shortest 
-        // path tree 
         int[] parents = new int[nVertices]; 
-  
-        // The starting vertex does not  
-        // have a parent 
         parents[startVertex] = NO_PARENT; 
-  
-        // Find shortest path for all  
-        // vertices 
         for (int i = 1; i < nVertices; i++) 
         { 
-  
-            // Pick the minimum distance vertex 
-            // from the set of vertices not yet 
-            // processed. nearestVertex is  
-            // always equal to startNode in  
-            // first iteration. 
             int nearestVertex = -1; 
             int shortestDistance = Integer.MAX_VALUE; 
             for (int vertexIndex = 0; vertexIndex < nVertices; vertexIndex++) 
@@ -116,14 +82,7 @@ public class dijkstra
                     shortestDistance = shortestDistances[vertexIndex]; 
                 } 
             } 
-  
-            // Mark the picked vertex as 
-            // processed 
             added[nearestVertex] = true; 
-  
-            // Update dist value of the 
-            // adjacent vertices of the 
-            // picked vertex. 
             for (int vertexIndex = 0; vertexIndex < nVertices; vertexIndex++)  
             { 
                 int edgeDistance = adjacencyMatrix[nearestVertex][vertexIndex]; 
@@ -136,17 +95,12 @@ public class dijkstra
                 } 
             } 
         } 
-  
         printSolution(startVertex, shortestDistances, parents); 
     } 
-  
-    // A utility function to print  
-    // the constructed distances 
-    // array and shortest paths 
+ 
     private static void printSolution(int startVertex, int[] distances, int[] parents) 
     { 
         int nVertices = distances.length; 
-          
         for (int vertexIndex = 0; vertexIndex < nVertices; vertexIndex++)  
         { 
             if (vertexIndex != startVertex)  
@@ -162,14 +116,8 @@ public class dijkstra
         } 
     } 
   
-    // Function to print shortest path 
-    // from source to currentVertex 
-    // using parents array 
     private static void printPath(int currentVertex, int[] parents) 
     { 
-          
-        // Base case : Source node has 
-        // been processed 
         if (currentVertex == NO_PARENT) 
         { 
             return; 
